13d8d231a419a64b064d8918caad304f81d0d3af
Matt Weeks <weekswmatt@gmail.com>
2023-03-27 16:03:56 -0600

    Cleaned up react warnings

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,13 +20,13 @@ const Home = () => {
       </div>
 
       <div className={classes.handsWrapper}>
-        <a href="https://github.com/m-weeks" target="_blank" className={[classes.pill, classes.redPill].join(' ')}>
-          <img src={redPill} />
+        <a href="https://github.com/m-weeks" target="_blank" rel="noreferrer" className={[classes.pill, classes.redPill].join(' ')}>
+          <img src={redPill} alt="Red Pill" />
         </a>
-        <a href="https://www.linkedin.com/in/matt-weeks-b61a0915b" target="_blank" className={[classes.pill, classes.bluePill].join(' ')}>
-          <img src={bluePill} />
+        <a href="https://www.linkedin.com/in/matt-weeks-b61a0915b" target="_blank" rel="noreferrer" className={[classes.pill, classes.bluePill].join(' ')}>
+          <img src={bluePill} alt="Blue Pill" />
         </a>
-        <img src={hands} className={classes.hands} />
+        <img src={hands} className={classes.hands} alt="Hands" />
       </div>
     </div>
   )
